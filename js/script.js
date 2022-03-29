8b962f571364b0037121f27f77a23975c8b239a8
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2022-03-28 21:49:08 -0400

    updating JS #2

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ canvas.height = 576;
 
 canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 class Sprite {
-  constructor(position) {
+  constructor({ position, velocity }) {
     this.position = position;
+    this.velocity = velocity;
   }
 
   draw() {
@@ -18,8 +19,24 @@ class Sprite {
 }
 
 const player = new Sprite({
-  x: 0,
-  y: 0,
+  position: {
+    x: 0,
+    y: 0,
+  },
 });
 player.draw();
+
+const enemy = new Sprite({
+  x: 400,
+  y: 100,
+});
+enemy.draw();
 console.log(player);
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  //   checking animate loop
+  console.log('animateLoop');
+}
+
+animate();
