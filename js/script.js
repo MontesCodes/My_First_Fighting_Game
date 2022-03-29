28c4acaa1274110aedf581ba99ff84ccdfe5dd06
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2022-03-28 20:26:15 -0400

    updating JS #1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+const canvas = document.querySelector('canvas');
+const canvasContext = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+class Sprite {
+  constructor(position) {
+    this.position = position;
+  }
+
+  draw() {
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(this.position.x, this.position.y, 50, 150);
+  }
+}
+
+const player = new Sprite({
+  x: 0,
+  y: 0,
+});
+player.draw();
+console.log(player);
