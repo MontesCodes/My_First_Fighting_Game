a612e6e53f11b2e2301e14e727d5a80ecd723edd
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2022-04-07 20:43:11 -0400

    Updating JS/CSS/HTML 4/7/22

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const canvasContext = canvas.getContext('2d');
 
 canvas.width = 1024;
 canvas.height = 576;
+// canvas.width = 1904;
+// canvas.height = 776;
 
 canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
