71c79996c3c80403a54d656a09aa48fde664b11b
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2022-04-28 20:37:52 -0400

    Updating JSx3/CSS/HTML 4/28/22 #1

--- js/script.js
+++ js/script.js
@@ -16,7 +16,17 @@ const background = new Sprite({
     x: 0,
     y: 0,
   },
-  imageSrc: '././img/background.png',
+  imageSrc: './img/background.png',
+});
+
+const shop = new Sprite({
+  position: {
+    x: 600,
+    y: 128,
+  },
+  imageSrc: './img/shop.png',
+  scale: 2.75,
+  framesMax: 6,
 });
 
 const player = new Fighter({
@@ -69,44 +79,6 @@ const keys = {
   },
 };
 
-function rectangularCollision({ rectangle1, rectangle2 }) {
-  return (
-    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
-      rectangle2.position.x &&
-    rectangle1.attackBox.position.x <=
-      rectangle2.position.x + rectangle2.width &&
-    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
-      rectangle2.position.y &&
-    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-  );
-}
-function determineWinner({ player, player2, timerId }) {
-  clearTimeout(timerId);
-  document.querySelector('#tie-displayText').style.display = 'flex';
-  if (player.health === player2.health) {
-    document.querySelector('#tie-displayText').innerHTML = 'Tie';
-  } else if (player.health > player2.health) {
-    document.querySelector('#tie-displayText').innerHTML = 'Player 1 Wins';
-  } else if (player.health < player2.health) {
-    document.querySelector('#tie-displayText').innerHTML = 'Player 2 Wins';
-  }
-}
-
-/// Timer Decreasing
-let timer = 60;
-let timerId;
-function decreaseTimer() {
-  if (timer > 0) {
-    timerId = setTimeout(decreaseTimer, 1000);
-    timer--;
-    document.querySelector('#timer').innerHTML = timer;
-  }
-  /// Tie match indicator
-
-  if (timer === 0) {
-    determineWinner({ player, player2, timerId });
-  }
-}
 decreaseTimer();
 /// Animation for my players
 function animate() {
@@ -114,6 +86,7 @@ function animate() {
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
+  shop.update();
   player.update();
   player2.update();
   player.velocity.x = 0;
